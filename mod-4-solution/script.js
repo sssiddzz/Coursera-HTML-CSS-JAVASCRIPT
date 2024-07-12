2b4f9a49880a31ae8c984b8d7c0b7e29ef229d04
sssiddzz <168815116+sssiddzz@users.noreply.github.com>
2024-07-12 22:46:56 +0530

    Update script.js

--- mod-4-solution/script.js
+++ mod-4-solution/script.js
@@ -1,22 +1,23 @@
+// Define byeSpeaker function
+function byeSpeaker(name) {
+    console.log("Goodbye " + name);
+}
 
+// Define helloSpeaker function
+function helloSpeaker(name) {
+    console.log("Hello " + name);
+}
 
+(function () {
+    var names = ["Siddharth", "Rohit", "Virat", "Hardik", "Jasprit", "Surya", "Rishabh", "Axar", "Gill", "Sanju", "Jimmy"];
 
-function () {
+    for (var i = 0; i < names.length; i++) {
+        var firstLetter = names[i].charAt(0).toLowerCase();
 
-  var names = ["Siddharth", "Rohit", "Virat", "Hardik", "Jasprit", "Surya", "Rishabh", "Axar", "Gill", "Sanju","Jimmy"];
-  
-  for (var i = 0; i < names.length; i++) {
-    var firstLetter = names[i].charAt(0).toLowerCase();
-  
-    if (firstLetter === 'j') {
-      byeSpeaker(names[i]);
+        if (firstLetter === 'j') {
+            byeSpeaker(names[i]);
+        } else {
+            helloSpeaker(names[i]);
+        }
     }
-    else {
-      helloSpeaker(names[i]);
-    }
-  }
-  
-  }
-  
-  
-  
+})();
